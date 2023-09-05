7a30f99ab3790b9f97ba11b66aef3f0c333eeb25
iHamza7 <khanazam3324@gmail.com>
2023-09-05 23:16:37 +0500

    lib changed

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         builder: (context, child) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'ToDo Applica',
+            title: 'ToDo Applicat',
             theme: ThemeData(
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
