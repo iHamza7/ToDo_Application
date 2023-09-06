963fa713d7ef38aa22fb56f201ec47f72bb5a244
iHamza7 <khanazam3324@gmail.com>
2023-09-06 09:38:54 +0500

    lib changed

--- lib/features/todo/pages/homepage.dart
+++ lib/features/todo/pages/homepage.dart
@@ -3,6 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sqflite_application/common/utils/constants.dart';
 import 'package:sqflite_application/common/widgets/appstyle.dart';
 import 'package:sqflite_application/common/widgets/reusable_text.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+import '../../../common/widgets/custom_text_field.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +15,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController search = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,10 +28,49 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 20.w),
-                  child: const Row(
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ReusableText(
+                        text: "Dashboard",
+                        style: appstyle(18, AppConst.kLight, FontWeight.bold),
+                      ),
+                      Container(
+                        width: 25.w,
+                        height: 25.h,
+                        decoration: const BoxDecoration(
+                          color: AppConst.kLight,
+                          borderRadius: BorderRadius.all(Radius.circular(9)),
+                        ),
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: const Icon(
+                            Icons.add,
+                            color: AppConst.kBkDark,
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
+                const SizedBox(height: 20),
+                CustomTextField(
+                  hintText: "Search",
+                  controller: search,
+                  suffixIcon: const Icon(FontAwesome.sliders),
+                  prefixIcon: Container(
+                    padding: EdgeInsets.all(14.h),
+                    child: GestureDetector(
+                      onTap: null,
+                      child: const Icon(
+                        AntDesign.search1,
+                        color: AppConst.kGreyLight,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
               ],
             )),
       ),
