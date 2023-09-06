fbc15efb7b5a31fc3b979d17a15f4470ef28ecb9
iHamza7 <khanazam3324@gmail.com>
2023-09-06 09:22:44 +0500

    lib changed

--- lib/features/todo/pages/homepage.dart
+++ lib/features/todo/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sqflite_application/common/utils/constants.dart';
 import 'package:sqflite_application/common/widgets/appstyle.dart';
 import 'package:sqflite_application/common/widgets/reusable_text.dart';
@@ -14,6 +15,22 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(85),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                ),
+              ],
+            )),
+      ),
       body: Center(
         child: ReusableText(
           text: 'Todo with riverpod',
