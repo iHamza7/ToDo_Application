348f9133eb9ea9f54c9dcd2a5c7a5ca154bf82f1
iHamza7 <khanazam3324@gmail.com>
2023-09-07 12:46:41 +0500

    lib changed

---
+++ lib/features/todo/pages/todo_tiles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../common/utils/constants.dart';
+import '../../../common/widgets/appstyle.dart';
+import '../../../common/widgets/reusable_text.dart';
+
+class TodoTile extends StatelessWidget {
+  final Color? color;
+  final String? title;
+  const TodoTile({super.key, this.color, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 8.h),
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8.h),
+            width: AppConst.kWidth,
+            decoration: BoxDecoration(
+                color: AppConst.kGreyLight,
+                borderRadius:
+                    BorderRadius.all(Radius.circular(AppConst.kRadius))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 5,
+                      decoration: BoxDecoration(
+                          //TODO: dynamic colors
+                          color: color ?? AppConst.kRed,
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(AppConst.kRadius))),
+                    ),
+                    const SizedBox(width: 15),
+                    Padding(
+                      padding: EdgeInsets.all(8.h),
+                      child: SizedBox(
+                        width: AppConst.kWidth,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ReusableText(
+                              text: title ?? "Title of TODO",
+                              style: appstyle(
+                                  18, AppConst.kLight, FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
