d698d66fb4be4ce8892eabe383697740cfd9002f
iHamza7 <khanazam3324@gmail.com>
2023-09-11 10:43:18 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 
+import 'common/utils/constants.dart';
 import 'features/todo/pages/homepage.dart';
 
 void main() {
@@ -11,6 +12,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  static final defaulLightColorScheme =
+      ColorScheme.fromSwatch(primarySwatch: Colors.blue);
+  static final defaulDarkColorScheme = ColorScheme.fromSwatch(
+      brightness: Brightness.dark, primarySwatch: Colors.blue);
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +31,13 @@ class MyApp extends StatelessWidget {
               debugShowCheckedModeBanner: false,
               title: 'ToDo Application',
               theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                scaffoldBackgroundColor: AppConst.kBkDark,
+                colorScheme: lightColorScheme ?? defaulLightColorScheme,
+                useMaterial3: true,
+              ),
+              darkTheme: ThemeData(
+                colorScheme: darkColorScheme ?? defaulDarkColorScheme,
+                scaffoldBackgroundColor: AppConst.kBkDark,
                 useMaterial3: true,
               ),
               themeMode: ThemeMode.dark,
