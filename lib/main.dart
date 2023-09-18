d6b743e95ee72f6b9a639b829d36c50e558e514e
iHamza7 <khanazam3324@gmail.com>
2023-09-18 23:12:27 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends ConsumerWidget {
               builder: (lightColorScheme, darkColorScheme) {
             return MaterialApp(
               debugShowCheckedModeBanner: false,
-              title: 'ToDo Application',
+              title: 'ToDo Applica',
               theme: ThemeData(
                 scaffoldBackgroundColor: AppConst.kBkDark,
                 colorScheme: lightColorScheme ?? defaulLightColorScheme,
