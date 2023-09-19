1ef9f9004ad687ef934465877c3ba89ca96553d1
iHamza7 <khanazam3324@gmail.com>
2023-09-19 19:38:28 +0500

    changed

--- lib/features/todo/pages/notification_helper.dart
+++ lib/features/todo/pages/notification_helper.dart
@@ -1,1 +1,14 @@
+import 'package:rxdart/rxdart.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+class NotificationHelper {
+  final WidgetRef ref;
+
+  NotificationHelper({required this.ref});
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  String? selectedNotificationPayload;
+  final BehaviorSubject<String?> selectNotificationSubject =
+      BehaviorSubject<String?>();
+}
